4025c0a1221bf5d90fa101a32dc25787abdcfd90
abdullah-adib <91241980+abdullah-adib@users.noreply.github.com>
2021-10-23 20:39:02 -0400

    added comments and made tweaks

--- client.py
+++ client.py
@@ -1,48 +1,50 @@
 from pynput import keyboard
 import socket
 
+# initialize port connections
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = socket.gethostname()
 port = 7634
 s.connect((host, port))
 
-speed = 0
+speed = 0   # initialize speed
 
 
 def on_press(key):
-    global speed
+    global speed    # makes speed usable in function
+    # Conditional statements for the keys used to control bot
     if format(key.char) == '0':
         speed = (0 / 5) * 255
-        print(f"speed has been set to {speed}")
+        print(f"speed has been set to 0")
         return speed
         s.send(key.char.encode())
     elif format(key.char) == '1':
         speed = ((1 / 5) * 255)
-        print(f"speed has been set to {speed}")
+        print(f"speed has been set to 1")
         return speed
         s.send(key.char.encode())
     elif format(key.char) == '2':
         speed = (2 / 5) * 255
-        print(f"speed has been set to {speed}")
+        print(f"speed has been set to 2")
         return speed
         s.send(key.char.encode())
     elif format(key.char) == '3':
         speed = (3 / 5) * 255
-        print(f"speed has been set to {speed}")
+        print(f"speed has been set to 3")
         return speed
         s.send(key.char.encode())
     elif format(key.char) == '4':
         speed = (4 / 5) * 255
-        print(f"speed has been set to {speed}")
+        print(f"speed has been set to 4")
         return speed
         s.send(key.char.encode())
     if format(key.char) == '5':
         speed = (5 / 5) * 255
-        print(f"speed has been set to {speed}")
+        print(f"speed has been set to 5")
         return speed
         s.send(key.char.encode())
-    elif format(key.char) == 'w':
+    if format(key.char) == 'w':
         print(f"[f{speed}]" * 4)
         s.send(key.char.encode())
     elif format(key.char) == 'a':
@@ -52,7 +54,7 @@ def on_press(key):
         print(f"[r{speed}]" * 4)
         s.send(key.char.encode())
     elif format(key.char) == 'd':
-        print(f"[f{speed}]" * 2 + f"[r{speed}]" * 2)
+        print(f"[f{speed}]"*2 + f"[r{speed}]" * 2)
         s.send(key.char.encode())
 
 
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import socket
-
+# initialize port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = socket.gethostname()
@@ -10,6 +10,33 @@ s.listen(1)
 
 con, addr = s.accept()
 print("connected with", addr)
-
+speed = 0   # initialize speed
 while True:
     c_messg = con.recv(1024)
+    # conditional statements
+    if c_messg.decode() == '0':
+        speed = (0 / 5) * 255
+        print(f"speed has been set to 0")
+    elif c_messg.decode() == '1':
+        speed = ((1 / 5) * 255)
+        print(f"speed has been set to 1")
+    elif c_messg.decode() == '2':
+        speed = (2 / 5) * 255
+        print(f"speed has been set to 2")
+    elif c_messg.decode() == '3':
+        speed = (3 / 5) * 255
+        print(f"speed has been set to 3")
+    elif c_messg.decode() == '4':
+        speed = (4 / 5) * 255
+        print(f"speed has been set to 4")
+    if c_messg.decode() == '5':
+        speed = (5 / 5) * 255
+        print(f"speed has been set to 5")
+    if c_messg.decode() == 'w':
+        print(f"[f{speed}]" * 4)
+    elif c_messg.decode() == 'a':
+        print(f"[r{speed}]" * 2 + f"[f{speed}]" * 2)
+    elif c_messg.decode() == 's':
+        print(f"[r{speed}]" * 4)
+    elif c_messg.decode() == 'd':
+        print(f"[f{speed}]"*2 + f"[r{speed}]" * 2)
